63c7a4aa7ff3f7ec4843e9886bd4af30e8c77b6e
Jaypal Patel <jaypalpatel713@gmail.com>
2024-09-08 15:55:20 +0530

    hero add

--- src/LandingPage/HomePage/Home.jsx
+++ src/LandingPage/HomePage/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero";
 
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
 export default Home;
