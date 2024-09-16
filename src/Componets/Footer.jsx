778139c8bc9202594c49775ad9f4a05600067a87
Anjali Tomar <160884550+Anjali3366@users.noreply.github.com>
2024-09-16 22:35:39 +0530

    color Change

--- src/Componets/Footer.jsx
+++ src/Componets/Footer.jsx
@@ -2,78 +2,11 @@ import React from "react";
 import { FaTwitter, FaInstagramSquare, FaWhatsapp } from "react-icons/fa";
 
 function Footer() {
-<<<<<<< Updated upstream
   return (
-    <div className="w-screen h-auto bg-blue-900 pt-8 text-center text-white ">
+    <div className="w-screen h-auto bg-[#0e3531] pt-8 text-center text-white ">
       <div>
         <h1 className=" text-start text-3xl font-bold text-[#dadada] px-20">
-=======
-
-  return <div className="w-screen h-80 bg-[#0e3531] p-5 pt-10 text-center text-white ">
-
-  <div className="grid grid-cols-12 text-center p-4">
-    <div className="col-span-2 m-2 p-3 bg-slate-200/10 h-auto gap-4 leading-6 ">
-    <a href="#" className="text-lg font-medium">Services</a>
-<ul className="text-sm leading-6 ">
-  <li>Get Room</li>
-  <li>Carpenter</li>
-  <li>Electrician</li>
-  <li>Plumber</li>
-</ul>
-
-
-    </div>
-  
-    <div className="col-span-2 m-2 p-3 bg-slate-200/10 h-auto gap-4 leading-6 ">
-    <a href="#" className="text-lg font-medium">Contact</a>
-<ul className="text-sm leading-6 ">
-  <li>About</li>
-  <li>Contact</li>
-  <li>Help</li>
-</ul>
-
-
-    </div>
-
-    <div className="col-span-3 m-2 p-3 bg-slate-200/10 h-auto gap-4 leading-6 ">
-    <a href="#" className="text-lg font-medium">What's Special</a>
-<ul className="text-sm leading-6 ">
-  <li>Easy Service</li>
-  <li>Opportunity for Worker</li>
-  <li>Solve daily Problem</li>
-</ul>
-
-
-    </div>
-
-
-    <div className=" col-span-3 w-full flex items-center px-4 py-2 rounded-3xl ">
-          <div className=" row-span-full flex items-center">
-          <input className="text-xl rounded  px-4 py-1
-           outline-none w-80 leading-8 shadow" placeholder="Search.." />
-       <button className=" w-20 text-xl mx-3 p-1 bg-amber-300 rounded text-white shadow">Search</button>
-       </div>
-       
-       <div  className=" row-span-full  text-lg">
-<FaTwitter className="text-4xl p-1" />
-<FaWhatsapp className="text-4xl p-1"/>
-<FaInstagramSquare className="text-4xl p-1" />
-
-</div>
-
-        
-
-
-  </div>
-
-  </div>
-
-  <hr  />
-
-  <div className="m-4 pl-12">
-        <h1 className=" text-start text-3xl font-bold text-[#dadada]">
->>>>>>> Stashed changes
-          Help<span className="text-amber-400">Hub</span>
+          Help<span className="text-amber-300">Hub</span>
         </h1>
       </div>
 
